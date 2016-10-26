6e22a58a18ecf6b20e36df9eb927704cdbb044fd
Fran Acien <amil101ftw@gmail.com>
2016-10-26 18:42:25 +0200

    Adding arduino script

---
+++ MesaBle/MesaBle.ino
@@ -0,0 +1,137 @@
+// Basic Bluetooth sketch HC-06_01
+// Connect the Hc-06 module and communicate using the serial monitor
+//
+// The HC-06 defaults to AT mode when first powered on.
+// The default baud rate is 9600
+// The Hc-06 requires all AT commands to be in uppercase. NL+CR should not be added to the command string
+//
+ 
+ 
+#include <SoftwareSerial.h>
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+int luz = 1;
+
+SoftwareSerial Serial1(10, 11); // RX, TX
+
+void setup() 
+{
+  
+    Serial.begin(9600);
+    Serial.println("Enter AT commands:");
+ 
+    // HC-06 default serial speed is 9600
+    Serial1.begin(9600);  
+
+    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+ 
+void loop()
+{
+    // Keep reading from HC-06 and send to Arduino Serial Monitor
+   // Keep reading from HC-06 and send to Arduino Serial Monitor
+  
+  Serial.println(luz);
+  
+  strip.show(); // This sends the updated pixel color to the hardware
+  delay(500);
+  rainbowCycle(20);
+ 
+}
+
+
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, DimColor(Wheel(((i * 255 / strip.numPixels()) + j) & 255)));
+    }
+    if (Serial1.available())
+    {  
+      String tmpString;
+      for(int i = 0; i< 3; i++){
+        tmpString += Serial1.read() - 48;
+      }
+      
+      luz = tmpString.toInt() / 12,5;
+      luz++;
+      Serial.println(luz);
+    }
+
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 100 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(100 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Return color, dimmed by 75% (used by scanner)
+    uint32_t DimColor(uint32_t color)
+    {
+        uint32_t dimColor = strip.Color(Red(color) >> luz, Green(color) >> luz, Blue(color) >> luz);
+        return dimColor;
+    }
+
+    // Returns the Red component of a 32-bit color
+    uint8_t Red(uint32_t color)
+    {
+        return (color >> 16) & 0xFF;
+    }
+ 
+    // Returns the Green component of a 32-bit color
+    uint8_t Green(uint32_t color)
+    {
+        return (color >> 8) & 0xFF;
+    }
+ 
+    // Returns the Blue component of a 32-bit color
+    uint8_t Blue(uint32_t color)
+    {
+        return color & 0xFF;
+    }
+
+
